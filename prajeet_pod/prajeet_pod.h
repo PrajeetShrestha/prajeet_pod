7e3cb83ac30ab4fc830fd3235951a121b2e3b74a
prajeet shrestha <prajeet.shrestha@gmail.com>
2022-09-01 14:14:46 +0545

    Initial commit

---
+++ prajeet_pod/prajeet_pod.h
@@ -0,0 +1,18 @@
+//
+//  prajeet_pod.h
+//  prajeet_pod
+//
+//  Created by Prajeet Shrestha on 01/09/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for prajeet_pod.
+FOUNDATION_EXPORT double prajeet_podVersionNumber;
+
+//! Project version string for prajeet_pod.
+FOUNDATION_EXPORT const unsigned char prajeet_podVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <prajeet_pod/PublicHeader.h>
+
+
